c4ee630304e84de8861e8aa2faf78a3c7e7fc0ef
viniciusraja <viniciusrfl@hotmail.com>
2023-11-22 10:17:18 -0300

    feat: create util to save my library into figma client storage

---
+++ app/components/MyLibraryPanel/utils/saveTranslationToMyLibrary.ts
@@ -0,0 +1,29 @@
+import { figmaAPI } from "@/lib/figmaAPI";
+
+const saveTranslationToMyLibrary = async (
+  originalTextProp: string = "",
+  customTranslationTextProp: string = ""
+) => {
+  try {
+    await figmaAPI.run(
+      async (figma, { originalText, customTranslationText }) => {
+        const itemToSaveToLibrary = { [originalText]: customTranslationText };
+        const myLibrary = await figma.clientStorage.getAsync("my-library");
+
+        if (!itemToSaveToLibrary) return;
+        await figma.clientStorage.setAsync("my-library", {
+          ...myLibrary,
+          ...itemToSaveToLibrary,
+        });
+      },
+      {
+        originalText: originalTextProp,
+        customTranslationText: customTranslationTextProp,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default saveTranslationToMyLibrary;
