8cf6cd15a476be85fe15424e8e0e19dd3d4d4f1d
viniciusraja <viniciusrfl@hotmail.com>
2023-11-27 10:07:24 -0300

    feat: create theme colors

---
+++ app/theme/colors.ts
@@ -0,0 +1,23 @@
+export interface StyleOptions {
+  colorMode: "light" | "dark";
+}
+
+const colors = {
+  primary: "#8400FF",
+  secondary: "#B600FF",
+  tertiary: "#DC00FF",
+  success: "#81C77B",
+  error: "#F18F8F",
+  warning: "#EADE76",
+  info: "#78CCD8",
+  white: "#ffffff",
+  text: "#444444",
+  gray: {
+    500: "#9A9A9A",
+    400: "#C7C7C7",
+    300: "#E8E8E8",
+  },
+  hover: "#EFEAF0",
+} as const;
+
+export default colors;
